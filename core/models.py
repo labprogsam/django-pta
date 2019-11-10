edcf7c964170666b3462eb43657d9ee7e383b21e
Samuel Miranda <som3@cin.ufpe.br>
2019-11-09 22:22:17 -0300

    Criação model Pessoa, ajuste na conf e exibição do HTML

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class People(models.Model):
+    name = models.CharField(verbose_name = 'Nome Completo', max_length = 100, null = True, blank=True)
+    cpf = models.IntegerField('CPF', primary_key = True)
+    email = models.EmailField('E-mail', null=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
